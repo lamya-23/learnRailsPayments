34ca734d7fcd068ead9ee5e2b448cf121ac30ac7
ouardirhi <lamyaa.ouardirhi@gmail.com>
2020-01-27 10:06:43 +0100

    generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnRailsPayments::Application.config.secret_token = '9e2543b7ea33bb0e24eb17b20f72c58ae1207a7038f9048979058d93ae283166994251ed204be26c7e1c27d45ec672418912b165fb4020d42b8c7a73621241fe'
